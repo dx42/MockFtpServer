bc319d4adf7d1f88c53f4eea1918e726660d0639
chrismair <chrismair@531de8e6-9941-0410-b38b-9a92acbe0330>
2008-11-11 22:20:57 +0000

    Convert main source from Groovy to Java

--- MockFtpServer/src/main/java/org/mockftpserver/core/IllegalStateException.java
+++ MockFtpServer/src/main/java/org/mockftpserver/core/IllegalStateException.java
@@ -1,52 +1,49 @@
 /*
  * Copyright 2008 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mockftpserver.core
-
-import org.mockftpserver.core.MockFtpServerException
+package org.mockftpserver.core;
 
 /**
- * Represents an error indicating that the current user has not yet logged in, but
- * is required to in order to invoke the requested command.
- * 
- * @version $Revision$ - $Date$
+ * Represents an error indicating that the server is in an illegal state, or
+ * that a server command is invoked when its preconditions have not been met.
  *
  * @author Chris Mair
+ * @version $Revision: 51 $ - $Date: 2008-05-09 22:16:32 -0400 (Fri, 09 May 2008) $
  */
-public class NotLoggedInException extends MockFtpServerException {
+public class IllegalStateException extends MockFtpServerException {
 
     /**
      * @param message
      */
-     NotLoggedInException(String message) {
-        super(message)
+    public IllegalStateException(String message) {
+        super(message);
     }
 
     /**
      * @param cause
      */
-     NotLoggedInException(Throwable cause) {
-        super(cause)
+    public IllegalStateException(Throwable cause) {
+        super(cause);
     }
 
     /**
      * @param message
      * @param cause
      */
-     NotLoggedInException(String message, Throwable cause) {
-        super(message, cause)
+    public IllegalStateException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
--- MockFtpServer/src/main/java/org/mockftpserver/core/NotLoggedInException.java
+++ MockFtpServer/src/main/java/org/mockftpserver/core/NotLoggedInException.java
@@ -1,52 +1,49 @@
 /*
  * Copyright 2008 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mockftpserver.core
-
-import org.mockftpserver.core.MockFtpServerException
+package org.mockftpserver.core;
 
 /**
- * Represents an error indicating that a server command has been received that
- * has invalid syntax. For instance, the command may be missing a required parameter.
- * 
- * @version $Revision$ - $Date$
+ * Represents an error indicating that the current user has not yet logged in, but
+ * is required to in order to invoke the requested command.
  *
  * @author Chris Mair
+ * @version $Revision: 51 $ - $Date: 2008-05-09 22:16:32 -0400 (Fri, 09 May 2008) $
  */
-public class CommandSyntaxException extends MockFtpServerException {
+public class NotLoggedInException extends MockFtpServerException {
 
     /**
      * @param message
      */
-     CommandSyntaxException(String message) {
-        super(message)
+    public NotLoggedInException(String message) {
+        super(message);
     }
 
     /**
      * @param cause
      */
-     CommandSyntaxException(Throwable cause) {
-        super(cause)
+    public NotLoggedInException(Throwable cause) {
+        super(cause);
     }
 
     /**
      * @param message
      * @param cause
      */
-     CommandSyntaxException(String message, Throwable cause) {
-        super(message, cause)
+    public NotLoggedInException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
