c84ac5de7b86f72a3c8060883e7998b46953a5e5
chrismair <chrismair@531de8e6-9941-0410-b38b-9a92acbe0330>
2008-11-16 01:01:35 +0000

    Convert main source from Groovy to Java

---
+++ src/main/java/org/mockftpserver/core/util/StringUtil.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mockftpserver.core.util;
+
+/**
+ * Contains static String-related utility methods.
+ *
+ * @author Chris Mair
+ * @version $Revision: 51 $ - $Date: 2008-05-09 22:16:32 -0400 (Fri, 09 May 2008) $
+ */
+public class StringUtil {
+
+    public static String padRight(String string, int width) {
+        int numSpaces = width - string.length();
+        return (numSpaces > 0) ? string + spaces(numSpaces) : string;
+    }
+
+    public static String padLeft(String string, int width) {
+        int numSpaces = width - string.length();
+        return (numSpaces > 0) ? spaces(numSpaces) + string : string;
+    }
+
+    private static String spaces(int numSpaces) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < numSpaces; i++) {
+            buf.append(" ");
+        }
+        return buf.toString();
+    }
+
+    /**
+     * Private constructor to prevent instantiation. All members are static.
+     */
+    private StringUtil() {
+    }
+
+}
