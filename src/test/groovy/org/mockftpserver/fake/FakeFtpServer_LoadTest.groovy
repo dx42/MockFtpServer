b4730e9624db0f47d0ffb3e418c620e4262564fb
chrismair <chrismair@531de8e6-9941-0410-b38b-9a92acbe0330>
2015-04-06 20:11:19 +0000

    #25: OutOfMemoryError; clean up closed sessions.

---
+++ src/test/groovy/org/mockftpserver/fake/FakeFtpServer_LoadTest.groovy
@@ -0,0 +1,77 @@
+package org.mockftpserver.fake
+
+import org.apache.commons.net.ftp.FTPClient
+import org.mockftpserver.test.AbstractGroovyTestCase
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+
+/*
+* Copyright 2014 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+class FakeFtpServer_LoadTest extends AbstractGroovyTestCase {
+
+    static final SERVER = "localhost"
+    static final PORT = 9981
+    static final USERNAME = 'joe'           // Must match Spring config
+    static final PASSWORD = 'password'      // Must match Spring config 
+
+    private FakeFtpServer fakeFtpServer
+    private long clientIndex = 0L
+
+    void testLotsOfClientConnections() {
+        startFtpServer('fakeftpserver-beans.xml')
+
+        final long NUM_CLIENTS = 50L
+        //final long NUM_CLIENTS = 500000L
+        NUM_CLIENTS.times {
+            startClientSession()
+        }
+    }
+
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+        super.tearDown()
+        fakeFtpServer?.stop()
+    }
+
+    private void startFtpServer(String springConfigFile) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(springConfigFile)
+        fakeFtpServer = (FakeFtpServer) context.getBean("fakeFtpServer")
+        fakeFtpServer.start()
+    }
+
+    private void startClientSession() {
+        long index = clientIndex++
+        log("Starting client #$index")
+
+        FTPClient ftpClient = new FTPClient()
+        ftpClient.connect(SERVER, PORT)
+        assert ftpClient.login(USERNAME, PASSWORD)
+
+        String dir = ftpClient.printWorkingDirectory()
+        assert dir == '/'
+
+        ftpClient.quit()
+
+        log("Finished client #$index")
+
+        assert fakeFtpServer.numberOfSessions() < 5
+    }
+
+}
